9d37c93cd6ba0a8a702e18dac793a7d1639cd177
dejan <dejandavidovict@gmail.com>
2022-09-30 21:05:00 +0200

    vueviews

---
+++ timesheet-app/src/components/Navigation/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Timesheets</a>
+  </li>
+  <li class="nav-item">
+    <RouterLink class="nav-link" to="/categories">Categories</RouterLink>
+  </li>
+  <li class="nav-item">
+    <RouterLink class="nav-link" to="/clients">Clients</RouterLink>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link">Projects</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link">Members</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link">Reports</a>
+  </li>
+</ul>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ timesheet-app/src/views/Categories.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Categories</h1>
+        <div v-for=" category in categories" v-bind:key="category.id">
+            <h2>{{ category.categoryname }}</h2>
+    </div>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            categories:[]
+        }
+    },
+    methods:{
+        async  getData(){
+           let response =  fetch("http://127.0.0.1:8000/api/Categories/")
+           this.categories = (await response).json();
+        }
+    },
+    mounted(){
+        this.getData();
+        console.log(this.categories);
+    }
+}
+</script>
+
+<style>
+
+</style>
